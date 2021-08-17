cc75cf2645b45b4e6c4d9b127cb9cff6ac96e4e2
HerryZheng <303983522@qq.com>
2021-08-17 12:36:34 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import x from './x.js'
-console.log('xxx');
-console.log('hi2')
+import jpg from './assets/1.jpg'
+
+const div = document.getElementById('head')
+
+div.innerHTML = `
+    <img src="${jpg}">
+`
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{
+        console.log('模块加载错误')
+    })
+}
+
+div.appendChild(button)
